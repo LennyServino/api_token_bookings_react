557f4fa9eae31406bc69946e89a2f0f77f36e6a8
Lenny <lennyservino@gmail.com>
2024-11-03 00:15:56 -0600

    agregando la primera parte de estilos al login

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -2,6 +2,12 @@ import React, { useEffect } from 'react'
 import { useForm } from 'react-hook-form'
 import { login } from '../services/loginServices';
 import { useNavigate } from 'react-router-dom';
+import styles from '../styles/Login.module.css'
+
+//importando icons
+import { FaSignInAlt, FaKey, FaLock, FaEnvelope  } from "react-icons/fa";
+import { IoInformationCircle } from "react-icons/io5";
+import { IoIosMail } from "react-icons/io";
 
 export default function Login() {
     const { register, handleSubmit } = useForm()
@@ -27,21 +33,33 @@ export default function Login() {
     }
 
     return (
-        <div>
-            <h1>Iniciar Sesion</h1>
-            <form action="" onSubmit={handleSubmit(loginForm)}>
-                <div>
-                    <label htmlFor="">Correo</label>
-                    <input type="email" {...register('email')}/>
-                </div>
-                <div>
-                    <label htmlFor="">Contraseña</label>
-                    <input type="password" {...register('password')} />
-                </div>
-                <div>
-                    <button type="submit">Iniciar Sesion</button>
-                </div>
-            </form>
+        <div className={styles.login_overlay}>
+            <div className={styles.login_content}>
+                <h1><FaSignInAlt /> Iniciar Sesion</h1>
+                <p><IoInformationCircle /> Ingresa tus credenciales para acceder al sistema</p>
+                <form action="" onSubmit={handleSubmit(loginForm)}>
+                    <div className={styles.input_box}>
+                        <label htmlFor="">Correo</label>
+                        <div className={styles.input_container}>
+                            <FaEnvelope className={styles.input_icon} />
+                            <input type="email" {...register('email')} placeholder='correo@ejemplo.com' />
+                        </div>
+                    </div>
+                    <div className={styles.input_box}>
+                        <div className={styles.password_box}>
+                            <label htmlFor="">Contraseña</label>
+                            <span><FaKey /> ¿Olvidaste tu contraseña?</span>
+                        </div>
+                        <div className={styles.input_container}>
+                            <FaLock className={styles.input_icon} />
+                            <input type="password" {...register('password')} placeholder='••••••••' />
+                        </div>
+                    </div>
+                    <div className={styles.btn_box}>
+                        <button type="submit"><FaSignInAlt /> Iniciar Sesion</button>
+                    </div>
+                </form>
+            </div>
         </div>
     )
 }

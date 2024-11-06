599e73276b05c5f123e8ac7f982702b4b65b1574
Lenny <lennyservino@gmail.com>
2024-11-05 23:52:10 -0600

    cambiando el app jsx para evitar conflictos en las rutas

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import { useEffect, useState } from 'react'
 /* import './App.css' */
 import Login from './components/Login'
 import Rutas from './routes/Rutas'
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
 import stylesMenu from './styles/Menu.module.css'
 import Menu from './routes/Menu'
-
+import Calendar from './components/Calendar'
 //importando iconos
 import { IoMenu } from "react-icons/io5";
+import PrivateRoute from './routes/privateRoute'
+import Accomodations from './components/Accomodations'
 
 function App() {
   const [menuOpen, setMenuOpen] = useState(false)
@@ -29,22 +31,34 @@ function App() {
   return (
     <>
       <BrowserRouter>
-        {
-          /* si el usuario esta muestro el dashboard */
-          user &&
-          <div className={stylesMenu.dashboard}>
-            <button className={stylesMenu.hamburger} onClick={toggleMenu}>
-              <IoMenu />
-            </button>
+        <div className={stylesMenu.dashboard}>
+          <button className={stylesMenu.hamburger} onClick={toggleMenu}>
+            <IoMenu />
+          </button>
 
-            <Menu className={`${stylesMenu.menu} ${menuOpen ? stylesMenu.active : 'hidden'}`} />
+          <Menu className={`${stylesMenu.menu} ${menuOpen ? stylesMenu.active : 'hidden'}`} />
 
-            {/* contenedor del contenido */}
-            <div className={stylesMenu.content}>
-              <Rutas user={user} />
-            </div>
+          {/* contenedor del contenido */}
+          <div className={stylesMenu.content}>
+            <Routes>
+              <Route path="/alojamientos" element={
+                <PrivateRoute>
+                  <Accomodations />
+                </PrivateRoute>
+              } />
+
+              <Route path="/calendario" element={
+                <PrivateRoute>
+                  <Calendar />
+                </PrivateRoute>
+              } /> 
+
+              <Route path="/" element={<Login />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="*" element={<Navigate to={user ? '/alojamientos' : '/login'} />} />
+            </Routes>
           </div>
-        }
+        </div>
       </BrowserRouter>
     </>
   )
